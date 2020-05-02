5b3ca288152cd2d8e9b96f6c9289e8966a26f1f0
Ricardo Jose Olvera Flores <ricardo.olvera.e@thalesdigital.io>
2020-05-02 12:38:02 -0500

    License added Compression of rda data file

--- sh/build.sh
+++ sh/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd ../../
+cd ../
 /usr/local/bin/R CMD build covid19mx
 /usr/local/bin/R CMD check covid19mx_0.1.0.tar.gz 
+/usr/local/bin/R CMD build --resave-data
+## Or in R
+R -e "
+setwd('../');
+devtools::build(pkg = 'covid19mx');
+devtools::check(pkg = 'covid19mx');
+"
